621a0ca378f08b05c8bbffceaf8c39807a1fa150
neptali <neptali@plerk.io>
2023-06-01 02:37:26 -0400

    adding python default env

--- options.lua
+++ options.lua
@@ -7,6 +7,7 @@ return {
     spell = false, -- sets vim.opt.spell
     signcolumn = "auto", -- sets vim.opt.signcolumn to auto
     wrap = false, -- sets vim.opt.wrap
+    colorcolumn = "90", -- set colorcolumn to 90
   },
   g = {
     mapleader = " ", -- sets vim.g.mapleader
@@ -16,6 +17,7 @@ return {
     diagnostics_mode = 3, -- set the visibility of diagnostics in the UI (0=off, 1=only show in status line, 2=virtual text off, 3=all on)
     icons_enabled = true, -- disable icons in the UI (disable if no nerd font is available, requires :PackerSync after changing)
     ui_notifications_enabled = true, -- disable notifications when toggling UI elements
+    python3_host_prog = "/home/neptali/miniconda3/bin/python3", -- set default python env
   },
 }
 -- If you need more control, you can use the function()...end notation

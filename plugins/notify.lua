b1e1d21d5e0d9ebba80f6683a4159ae143824bfc
neptali <neptali@plerk.io>
2023-06-01 18:28:12 -0400

    fix background notify error

---
+++ plugins/notify.lua
@@ -0,0 +1,15 @@
+return {
+    {
+    "rcarriga/nvim-notify",
+    config = function(plugin, opts)
+        -- run the core AstroNvim configuration function with the options table
+        require("plugins.configs.notify")(plugin, opts)
+
+        -- require telescope and load extensions as necessary
+        require('notify').setup ({
+        -- other stuff
+            background_colour = "#000000"
+        })
+    end,
+  },
+}

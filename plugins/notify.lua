b719381e08bbdeaf98738add7cf3ed591ee7b832
neptali <njpm28@gmail.com>
2023-09-16 00:27:01 -0400

    changing notify config

--- plugins/notify.lua
+++ plugins/notify.lua
@@ -1,15 +1,10 @@
 return {
-    -- {
-    --     "rcarriga/nvim-notify",
-    --     config = function(plugin, opts)
-    --         -- run the core AstroNvim configuration function with the options table
-    --         require("plugins.configs.notify")(plugin, opts)
-    --
-    --         -- require telescope and load extensions as necessary
-    --         require('notify').setup({
-    --             -- other stuff
-    --             background_colour = "#000000"
-    --         })
-    --     end,
-    -- },
+  {
+    "rcarriga/nvim-notify",
+    -- require telescope and load extensions as necessary
+    require('notify').setup({
+      -- other stuff
+      background_colour = "#000000"
+    })
+  },
 }
